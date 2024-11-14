6bd3753f887de3b1495472afc78308610878ada2
Aadi <goooolok727@gmail.com>
2024-11-15 04:04:36 +0530

    added so many things

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import "./style.css";
 
 console.log("Radhey Shyam");
 
+interface Selection {
+	start: number;
+	end: number;
+}
+
 class Mountable {
 	constructor(protected _view: HTMLElement = document.createElement("div")) {}
 
@@ -10,12 +15,12 @@ class Mountable {
 		return this._view;
 	}
 
-	connectedCallback() {}
+	init() {}
 
 	mount(container: HTMLElement) {
 		container.append(this._view);
 		requestAnimationFrame(() => {
-			this.connectedCallback();
+			this.init();
 		});
 	}
 }
@@ -37,76 +42,163 @@ class ResultView extends Mountable {
 		if (resultThing) resultThing.textContent = res;
 	}
 }
+
 class ContentEditable extends Mountable {
-	private text: string;
+	private text = "";
+	private selection: Selection | null = null;
+
 	constructor(private highligher: Highlighter) {
 		super();
+		this.selection = null;
+
 		this._view.classList.add("formula-editor-content-editable");
-		this._view.innerText = "a = 10";
+		this._view.style.whiteSpace = "pre";
+		this._view.style.wordBreak = "break-word";
 		this._view.contentEditable = "true";
-		this.text = `
-			a = false
-			true false
-			b = 10.0
-			thing = "thing"
-			a = 'abcd'
-			thing()
-			a.thing()
-			a + b
-		`;
+		this._view.style.outline = "none";
+		this._view.spellcheck = false;
+		this._view.innerText = "";
 	}
 
 	get view() {
 		return this._view;
 	}
 
-	connectedCallback(): void {
-		requestAnimationFrame(() => {
-			this.render();
+	override init(): void {
+		// TODO dispose
+		this._view.addEventListener("input", () => {
+			this.storeSelection();
+			requestAnimationFrame(() => {
+				this.render();
+				this.restoreSelection();
+			});
 		});
+
+		document.addEventListener("selectionchange", this.storeSelection);
+
+		this._view.focus();
+
+		this.render();
 	}
 
-	update() {
-		this.text = this._view.innerText ?? "";
+	setSelection(start: number, end: number, sync = true) {
+		this.selection = { start, end };
+		if (sync) this.restoreSelection();
 	}
+
+	private convertSelectionToRange(selection: Selection) {
+		const range = document.createRange();
+		// TODO optimize collapsed
+		const start = this.findTextNode(selection.start);
+		const end = this.findTextNode(selection.end);
+		if (start && end) {
+			range.setStart(start.node, start.offset);
+			range.setEnd(end.node, end.offset);
+			return range;
+		}
+		return null;
+	}
+
+	private findTextNode(index: number) {
+		const allUnits = Array.from(
+			this._view.querySelectorAll("[data-offset]"),
+		) as HTMLElement[];
+
+		// Binary search
+		let low = 0;
+		let high = allUnits.length - 1;
+
+		while (low <= high) {
+			const mid = Math.floor((low + high) / 2);
+			const unit = allUnits[mid];
+			const dataOffset = Number.parseInt(unit.dataset.offset as string);
+			const length = unit.textContent?.length ?? 0;
+
+			if (index >= dataOffset && index <= dataOffset + length) {
+				return {
+					node: unit.firstChild as Node,
+					offset: index - dataOffset,
+				};
+			}
+
+			if (index < dataOffset) {
+				high = mid - 1;
+			} else {
+				low = mid + 1;
+			}
+		}
+
+		return null;
+	}
+
+	private restoreSelection() {
+		if (this.selection) {
+			const range = this.convertSelectionToRange(this.selection);
+			const sel = document.getSelection();
+			if (range) {
+				sel?.removeAllRanges();
+				sel?.addRange(range);
+			}
+		}
+	}
+
+	storeSelection = () => {
+		const sel = window.getSelection();
+
+		if (sel?.rangeCount) {
+			const range = sel.getRangeAt(0);
+
+			const calcOffset = (node: Node) => {
+				const unit = node.parentElement?.closest(
+					"[data-formula-unit]",
+				) as HTMLElement | null;
+
+				return Number.parseInt(unit?.dataset.offset ?? "0");
+			};
+
+			const startOffset = calcOffset(range.startContainer);
+			const endOffset = calcOffset(range.endContainer);
+			this.selection = {
+				start: startOffset + range.startOffset,
+				end: endOffset + range.endOffset,
+			};
+		}
+	};
+
+	dispose() {}
 
 	render() {
-		const code = this.text
-			.trim()
-			.split("\n")
-			.map((l) => l.trim())
-			.join("\n");
+		this.text = this._view.innerText ?? "";
 
-		const normalized = code.replaceAll(/\r\n/g, "\n");
-
-		const tokens = this.highligher.codeToTokensBase(normalized, {
+		const code = this.text;
+		const lines = this.highligher.codeToTokensBase(code, {
 			// biome-ignore lint/suspicious/noExplicitAny: <explanation>
 			lang: "formula" as any,
 			theme: "ayu-dark",
 		});
 
-		const html = this._getHtml(tokens);
-		this._view.innerHTML = html;
-	}
-	private _getHtml(tokens: ThemedToken[][]) {
-		const lines = tokens.map((line) => {
-			return `<p>${line.map((t) => `<span style="color: ${t.color};">${t.content}</span>`).join("")}</p>`;
-		});
-		console.log(lines);
-		return lines.join("");
-	}
+		let offset = 0;
+		const createSpan = (content: string, color = "") => {
+			const span = document.createElement("span");
 
-	setSelection() {
-		requestAnimationFrame(() => {
-			const sel = document.getSelection();
-			if (!sel) return;
+			span.setAttribute("data-formula-unit", "true");
+			span.setAttribute("data-offset", offset.toString());
+			offset += content.length;
+			span.classList.add("formula-editor-unit");
+			span.style.color = color;
+			span.innerText = content;
+			return span;
+		};
 
-			const lastChild = this._view.lastChild;
-			sel.setPosition(
-				lastChild ?? this._view,
-				lastChild ? (lastChild.textContent?.length ?? 0) : 0,
-			);
-		});
+		const items = [];
+		for (const units of lines) {
+			for (const unit of units) {
+				const span = createSpan(unit.content, unit.color);
+				items.push(span);
+			}
+			if (units.length !== 0) items.push(createSpan("\n"));
+		}
+		this._view.replaceChildren(...items);
 	}
 }
 
@@ -132,15 +224,6 @@ class FormulaEditor extends Mountable {
 		this._view.append(this.header);
 		this.inlineEditor.mount(this.header);
 		this.result.mount(this._view);
-
-		// for testing
-		const button = document.createElement("button");
-		button.innerText = "Update";
-		this._view.append(button);
-		button.addEventListener("click", () => {
-			this.inlineEditor.update();
-			this.inlineEditor.render();
-		});
 	}
 }
 
@@ -155,9 +238,9 @@ async function createFormulaHighlighter() {
 	return highligher;
 }
 
-(async () => {
-	const highlighter = await createFormulaHighlighter();
-
-	const editor = new FormulaEditor(highlighter);
-	editor.mount(document.body);
-})();
+createFormulaHighlighter()
+	.then((highlighter) => {
+		const editor = new FormulaEditor(highlighter);
+		editor.mount(document.body);
+	})
+	.catch(console.error);
